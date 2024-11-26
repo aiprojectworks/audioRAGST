24cf539ee1385b7d336acac538aa0fde0a07cccb
AutumnBoxes <yongsiu32123@gmail.com>
2024-11-26 16:59:33 +0800

    i swaer if this doesnt work

--- main.py
+++ main.py
@@ -326,7 +326,7 @@ def handle_userinput(user_question):
 def displayPDF(file):
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="600" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}#embedded=true" width="700" height="600" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
