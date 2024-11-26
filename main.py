b9ac140f009339fa1f4ddfe9a424171af4e30bcf
AutumnBoxes <yongsiu32123@gmail.com>
2024-11-26 17:18:37 +0800

    man i dunno

--- main.py
+++ main.py
@@ -326,7 +326,13 @@ def handle_userinput(user_question):
 def displayPDF(file):
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}#embedded=true" width="700" height="600" type="application/pdf"></iframe>'
+    # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="600" type="application/pdf"></iframe>'
+    pdf_display = f"""<embed
+       class="pdfobject"
+       type="application/pdf"
+       title="Embedded PDF"
+       src="data:application/pdf;base64,{base64_pdf}"
+       style="overflow: auto; width: 100%; height: 100%;">"""
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
